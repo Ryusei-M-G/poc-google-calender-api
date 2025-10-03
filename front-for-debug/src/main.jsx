10c8f1222b17c2f65f90a2211bc8ca6cef268ce8
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-10-03 11:24:35 +0900

    AuthProviderを使用してRouterをラップし、認証状態を管理するためのコンテキストを追加

--- front-for-debug/src/main.jsx
+++ front-for-debug/src/main.jsx
@@ -1,19 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './AuthProvider.jsx'
 import App from './App.jsx'
 import SuccessPage from './successPage.jsx'
 import Form from './Form.jsx'
 import Navigate from './Navigate.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Navigate />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="auth/success" element={<SuccessPage />} />
-        <Route path='/form' element={<Form />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navigate />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="auth/success" element={<SuccessPage />} />
+          <Route path='/form' element={<Form />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </StrictMode>,
 )
